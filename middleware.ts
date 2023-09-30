609a7ad1189b1c4191d8eaf198a72d84db2e1eb4
seungmin-dev <seungmin.dev@gmail.com>
2023-09-30 13:16:09 +0900

    Style: 미들웨어 주석 수정

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,9 @@
-// import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
 
-const secret = process.env.SECRET_KEY;
-
-// export default withAuth(
 export async function middleware(req: NextRequest) {
-  // const token = await getToken({ req, secret, raw: true });
   const { pathname } = req.nextUrl;
   let token;
-
-  console.log("req : ", req);
-  console.log("req.cookies : ", req.cookies);
-  console.log("req.cookies._parsed : ", req.cookies._parsed);
 
   if (req.cookies._parsed.get("_vercel_jwt")) {
     token = req.cookies._parsed.get("_vercel_jwt")?.value;
@@ -29,9 +20,6 @@ export async function middleware(req: NextRequest) {
   console.log("------------------------------");
 
   if (token && pathname.startsWith("/login")) {
-    console.log("------------------------------");
-    console.log("로딩 페이지로 이동...");
-    console.log("------------------------------");
     return NextResponse.redirect(new URL("/loading", req.url));
   }
   if (
@@ -43,13 +31,9 @@ export async function middleware(req: NextRequest) {
     console.log("token : ", token, " pathname : ", pathname);
     console.log("------------------------------");
 
-    console.log("------------------------------");
-    console.log("곧 login으로 리다이렉트 ...");
-    console.log("------------------------------");
     return NextResponse.redirect(new URL("/login", req.url));
   }
 }
-// );
 
 export const config = {
   matcher: ["/", "/login", "/api/:auth*"],
